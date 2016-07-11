612cc76ece42ac086ca96346b69de554f301a148
Zhong Jiasheng <770649662@qq.com>
2016-07-11 22:42:08 +0800

    init home

---
+++ C/chain_high_level.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define NAME_LEN
+
+struct part{
+
+	int number;
+	char name[NAME_LEN + 1];
+	int on_hand;
+
+	struct part *next;
+}
+
+
+int main(){
+
+	
+
+	return 0;
+}
+
