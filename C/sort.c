cc1271a2f1eaba57f13ddb814910e0f61e61f63b
Zhong Jiasheng <770649662@qq.com>
2016-11-17 23:13:56 +0800

    1117

--- C/sort.c
+++ C/sort.c
@@ -6,7 +6,7 @@ void sort_insert1();
 int main(){
 
 	printf("hello World\n");
-	
+
 	sort_insert1();
 
 	return 0;
@@ -15,12 +15,12 @@ int main(){
 int arr[7] = {49,38,65,97,76,13,27};
 
 void printArr(){
-	
+
     for(int i = 0; i < 7; i++){
-    
+
 	printf("%d,",arr[i]);
     }
-	
+
     printf("\n");
 }
 
@@ -28,20 +28,20 @@ void sort_insert1(){
 
     printf("排序前：");
     printArr();
-	
+
     for (int i = 1; i < 7; i ++){
-    
+
     	for(int j = i; j > 0; j--){
-	
+
 		if(arr[j] < arr[j-1]){
-		
+
 			int tmp = arr[j];
 			arr[j] = arr[j-1];
 			arr[j-1] = tmp;
 
 		}
 	}
-    
+
     }
 
     printf("排序后：");
@@ -53,28 +53,28 @@ void sort_insert1(){
 void sort_insert(){
 
 	printf("排序前:");
-    	printArr();	
+    	printArr();
 
 	for(int i = 1; i < 7; i++){
-	
+
 		for(int j = 0; j < i; j++){
-		
+
 			if(arr[j] > arr[i]){
-				
+
 			    	int tmp = arr[i];
-			    	
+
 			    	for(int k = i; k > j; k--){
-				
+
 					arr[k] = arr[k-1];
 				}
-			    	
+
 	    			arr[j] = tmp;
-	    		
+
 				printArr();
 
-				break;	
+				break;
 			}
-			
+
 		}
 
 	}
--- Ruby/test.rb
+++ Ruby/test.rb
@@ -8,4 +8,17 @@ print <<EOF
 	多行字符串
 EOF
 
+for i in 0..5
+       if i < 2 then
+	         next
+		    end
+          puts "局部变量的值为 #{i}"
+end
+
+ARRAY = ["O","K"];
+$string = "";
+ARRAY.each do [i] 
+ $string += ARRAY[i];
+end
+put $string;
 
