d9108a987ed0d19e926e89e94dfb210a974a53a6
Zhong Jiasheng <770649662@qq.com>
2016-11-17 23:22:04 +0800

    1117

---
+++ Note/摘要11月.c
@@ -0,0 +1,37 @@
+
+
+11月15日
+1、确定以后的学习方向还是以iOS为主，其他东西为辅的策略
+2、atom使用git的一些建议："http://blog.csdn.net/u010494080/article/details/51229211"
+3、atom插件管理命令apm: "http://blog.csdn.net/u010494080/article/details/50605654"
+4、对廖雪峰教程的一个不错的总结，且和atom相关："http://www.jianshu.com/p/7efdaf04e44c"
+
+11月14日
+1、开始弄PHP爬虫项目
+
+11月13日
+1、配置phpStorm
+2、下载MAMP
+3、安装xdebug插件
+
+11月7日
+1、axure原型设计："http://www.jikexueyuan.com/course/1447_1.html?ss=1"
+2、
+
+11月6日
+
+1、ngnix操作步骤：目录：/usr/local/etc/nginx/
+1、ngnix
+
+2、mySql操作步骤：
+1>mysql.server start
+2>mysql -h 172.16.16.45 -P 3306 -u root -p123
+
+3、php-fpm操作步骤：目录：/private/etc/
+1>killall php-fpm
+2>sudo php-fpm
+
+11月2日
+1、配置ngnix+php,极客学院："http://www.jikexueyuan.com/course/1847_1.html" 3个视频
+2、连接mySql，操作数据库："http://www.jikexueyuan.com/course/457_2.html"
+3、php和html混合编程: "http://www.jikexueyuan.com/course/457_3.html?ss=1"
